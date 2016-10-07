e19f1f4c2420eb0929d67fc2bd74128167e59951
Chris Alfano <chris@jarv.us>
2016-10-07 15:43:09 -0400

    Add build for v1.0.3

--- sencha-workspace/packages/scienceleadership-interims/build/scienceleadership-interims-debug.js
+++ sencha-workspace/packages/scienceleadership-interims/build/scienceleadership-interims-debug.js
@@ -1,5 +1,33 @@
 Ext.define('Scienceleadership.interims.overrides.InterimsController', {
     override: 'SlateAdmin.controller.progress.Interims',
+    init: function() {
+        var me = this,
+            editorForm, notesField, archivedNotesField;
+        me.callParent();
+        me.on('beforereportload', function(report) {
+            var isHtml = report.get('NotesFormat') == 'html';
+            if (!editorForm) {
+                editorForm = me.getEditorForm().getForm();
+                notesField = editorForm.findField('Notes');
+                archivedNotesField = editorForm.findField('ArchivedNotes');
+            }
+            notesField.setHidden(isHtml);
+            archivedNotesField.setHidden(!isHtml);
+            archivedNotesField.setValue(isHtml ? report.get('Notes') : null);
+        });
+    },
+    syncFormButtons: function() {
+        var me = this,
+            editorForm = me.getEditorForm(),
+            report = editorForm.getRecord();
+        if (report && report.get('NotesFormat') == 'html') {
+            me.getRevertChangesBtn().setDisabled(true);
+            me.getSaveDraftBtn().setDisabled(true);
+            me.getSaveFinishedBtn().setDisabled(true);
+        } else {
+            me.callParent();
+        }
+    },
     syncStudent: function(student) {
         this.callParent(arguments);
         var report = student.get('report');
@@ -13,7 +41,7 @@ Ext.define('ScienceLeadership.interims.overrides.InterimsEditorForm', {
     override: 'SlateAdmin.view.progress.interims.EditorForm',
     requires: [
         'Ext.form.field.ComboBox',
-        'Ext.form.field.TextArea'
+        'Ext.form.field.Display'
     ],
     initComponent: function() {
         var me = this;
@@ -28,6 +56,13 @@ Ext.define('ScienceLeadership.interims.overrides.InterimsEditorForm', {
                     'F',
                     'N/A'
                 ]
+            },
+            {
+                xtype: 'displayfield',
+                name: 'ArchivedNotes',
+                fieldLabel: 'Comments (archived)',
+                labelAlign: 'top',
+                hidden: true
             }
         ]);
         me.callParent(arguments);
