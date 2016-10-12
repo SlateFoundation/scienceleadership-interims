8f6ede816450a22c2c46c1c2f71f9b8f6e481c38
Chris Alfano <chris@jarv.us>
2016-10-12 11:02:40 -0400

    Add complete set of letter grades

--- php-config/Slate/Progress/SectionInterimReport.config.d/scienceleadership-interims.php
+++ php-config/Slate/Progress/SectionInterimReport.config.d/scienceleadership-interims.php
@@ -4,7 +4,7 @@ namespace Slate\Progress;
 
 SectionInterimReport::$fields['Grade'] = [
     'type' => 'enum',
-    'values' => ['D', 'F', 'N/A'],
+    'values' => ['A', 'B', 'C', 'D', 'F', 'N/A'],
     'default' => null
 ];
 
