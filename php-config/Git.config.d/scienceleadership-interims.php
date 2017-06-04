4a05f15d8cb2068584f1686f70b4a742d3d4a2be
Chris Alfano <chris@jarv.us>
2017-06-04 16:46:40 +0000

    Add email template and update git config

--- php-config/Git.config.d/scienceleadership-interims.php
+++ php-config/Git.config.d/scienceleadership-interims.php
@@ -5,6 +5,9 @@ Git::$repositories['scienceleadership-interims'] = [
     'originBranch' => 'builds/v1',
     'workingBranch' => 'builds/v1',
     'trees' => [
+        'html-templates/scienceleadership-interims/_body.tpl',
+        'html-templates/scienceleadership-interims/_body.email.tpl',
+        'php-config/Git.config.d/scienceleadership-interims.php',
         'php-config/Slate/Progress/SectionInterimReport.config.d/scienceleadership-interims.php',
         'php-migrations/ScienceLeadership/Interims',
         'sencha-workspace/packages/scienceleadership-interims'
